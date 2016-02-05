73b7bc3d7a4ec1c840be899f02475039d75635f2
Richard ANDRINIRINA <randrini@student.42.fr>
2016-02-05 21:01:50 +0100

    Go

---
+++ srcs/parse_file.c
@@ -0,0 +1,185 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_to_grid.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: randrini <randrini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/31 15:37:00 by randrini          #+#    #+#             */
+/*   Updated: 2016/02/05 17:52:39 by randrini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lib.h"
+
+int ft_error(void)
+{
+    ft_putstr("map error\n");
+    exit (1);
+}
+
+char    *first_line(char *buf)
+{
+    char    *first_line;
+    int i;
+
+    i = 0;
+    if (buf[0] == '\0')
+        ft_error();
+    while (buf[i] != '\n')
+        i++;
+    first_line = (char*)malloc(sizeof(char) * (i + 1));
+    i = 0;
+    while (buf[i] != '\n')
+    {
+        first_line[i] = buf[i];
+        i++;
+    }
+    first_line[i] = '\0';
+    return (first_line);
+}
+
+int     ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+int     ft_intlen(int n)
+{
+
+    int i;
+    
+    i = 0;
+    while ( n != 0)
+    {
+        n /= 10;
+        ++i;
+    }
+    return (i);
+}
+
+int     ft_strlen_mod(char *str)
+{
+    int i;
+    i = 0;
+    while (str[i] != '\n')
+        i++;
+    return (i);
+}
+
+void    ft_check_file(char *buf)
+{
+    int i;
+    char *str;
+    t_param *tmp_param;
+
+    i = 0;
+    str = first_line(buf);
+    tmp_param = ft_get_parameters(buf);
+    if (str[0] >= 48 && str[0] <= 57)
+    {
+        str[ft_strlen_mod(buf) - 3] = '\0';
+        if (ft_atoi(str) != size_col(buf))
+            ft_error();
+        i = ft_intlen(ft_atoi(str));
+        str = first_line(buf);    
+        if (str[i] == tmp_param->empty)
+            i++;
+        if (str[i] == tmp_param->obst)
+            i++;
+        if (str[i] == tmp_param->full)
+            i++;
+        else
+            ft_error();
+    }
+    else
+        ft_error();     
+}
+
+void    ft_check_grid(char *str) //grid width check
+{
+    int i;
+    int j;;
+    int k;
+
+    i = 0;
+    j = 0;
+    while (str[i] != '\n')
+        i++;
+    i += 1;
+    while (str[++i] != '\n' && str[i] != '\0')
+        j++;
+    i += 1;
+    k = i;
+    while (str[i] != '\0')
+    {
+        while (str[i] != '\n' && str[i] != '\0')
+            ++i;
+        if ((i - 1 - k) != j)
+            ft_error();
+        else
+        {
+           i++;
+            k = i;
+        }
+    }
+}
+
+void    ft_check_char(char *buf)
+{
+    int i;
+    t_param *tmp_param;
+
+    i = skip_first_line(buf);
+    tmp_param = ft_get_parameters(buf);
+    while (buf[i] != '\0')
+    {
+        if (buf[i] == tmp_param->empty || buf[i] == tmp_param->obst || buf[i] == '\n')
+            i++;
+        else
+            ft_error();
+    }
+}
+
+int ft_check_full_o(char *str)
+{
+    int i;
+    
+    i = 0;
+    while (str[i] != '\n')
+        i++;
+    i++;
+    while (str[i] != '\0')
+    {
+        if (str[i] == 'o' || str[i] == '\n')
+            i++;
+        else
+            return (0);
+    }
+    ft_error();
+    return (1);
+}
+
+char    *parse_file(char *file)
+{
+	int         fd;
+	int         ret;
+	static char buf[BUFFER_SIZE];
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		ft_error();
+	ret = read(fd, buf, BUFFER_SIZE);
+	buf[ret] = '\0';
+	close(fd);
+    ft_check_file(buf);
+    ft_check_char(buf);
+    ft_check_full_o(buf);
+    ft_check_grid(buf);
+	return (buf);
+}
